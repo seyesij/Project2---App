0d5d05691ad4bef29683404dbe1c5771489abbad
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-01 10:39:04 -0400

    initial commit

---
+++ db/seeds/seed.sql
@@ -0,0 +1,2 @@
+\c shows_app_dev;
+\i shows.sql;
---
+++ db/seeds/shows.sql
@@ -0,0 +1,29 @@
+INSERT INTO shows (title, genre, country, network, status) VALUES
+  (
+    'Empire',
+    'Musical Drama',
+    'US',
+    'FOX',
+    'Running'
+  ),
+  (
+    'Love Island',
+    'Reality TV',
+    'UK',
+    'ITV2',
+    'Running'
+  ),
+  (
+    'Geordie Shore',
+    'Reality TV',
+    'UK',
+    'MTV',
+    'Running'
+  ),
+  (
+    'Damages',
+    'Crime',
+    'US',
+    'Audience network',
+    'Ended'
+  );
