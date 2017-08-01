cd8a61a4dbc353b9e5e544f3c0b6be42f982dca4
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-01 11:54:14 -0400

    inital commit

---
+++ db/migrations/migration-00032017.sql
@@ -0,0 +1,28 @@
+\c shows_app_dev;
+
+CREATE TABLE IF NOT EXISTS users
+(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  firstname VARCHAR(255),
+  lastname VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS shows
+(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  country VARCHAR(255),
+  network VARCHAR(255),
+  status VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users_shows
+(
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  show_id INT REFERENCES shows(id)
+);
